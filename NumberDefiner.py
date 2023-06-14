1958f5511b4910661890bb329f3563a4d930df33
TomiVerano <125672988+TomiVerano@users.noreply.github.com>
2023-06-14 17:39:37 +0300

    Add files via upload

--- NumberDefiner.py
+++ NumberDefiner.py
@@ -1,9 +1,3 @@
-# Write a program that reads a floating-point number and:
-# prints "zero" if the number is zero
-# prints "positive" or "negative"
-# adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds
-# 1 000 000
-
 number_to_define = float(input())
 if number_to_define == 0:
     print('zero')
